7ed1def5ade5f98d6a018fd748d3dc0be8a4774c
Sam Carlberg <sam.carlberg@gmail.com>
2017-05-28 00:25:53 -0400

    Fix deletes and persistence
    ntcore is really inconsistent. sometimes it wants a leading slash, sometimes it doesn't

--- src/main/java/edu/wpi/first/tableviewer/NetworkTableUtils.java
+++ src/main/java/edu/wpi/first/tableviewer/NetworkTableUtils.java
@@ -8,10 +8,30 @@ import edu.wpi.first.wpilibj.tables.ITable;
  */
 public class NetworkTableUtils {
 
-  public static final ITable rootTable = NetworkTable.getTable("");
-
   public static String normalize(String key) {
     return ("/" + key).replaceAll("/{2,}", "/");
+  }
+
+  public static String concat(String key1, String key2, String... more) {
+    StringBuilder b = new StringBuilder(key1).append('/').append(key2);
+    for (String s : more) {
+      b.append('/').append(s);
+    }
+    return normalize(b.toString());
+  }
+
+  public static String simpleKey(String key) {
+    if (key.isEmpty() || key.equals("/")) {
+      return "Root";
+    }
+    if (!key.contains("/")) {
+      return key;
+    }
+    return key.substring(key.lastIndexOf('/') + 1);
+  }
+
+  public static ITable getRootTable() {
+    return NetworkTable.getTable("");
   }
 
 }
