52708ed9c2c7fac9489094814ff3cc7b93e0cd30
Sam Carlberg <sam.carlberg@gmail.com>
2017-05-29 19:34:09 -0400

    Add tests for entry classes and preferences controller

---
+++ src/test/java/edu/wpi/first/tableviewer/entry/BooleanArrayEntryTest.java
@@ -0,0 +1,33 @@
+package edu.wpi.first.tableviewer.entry;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class BooleanArrayEntryTest {
+
+  @Test
+  public void testTypeStringEmpty() {
+    assertEquals("Boolean[0]", new BooleanArrayEntry("", new boolean[0]).getType());
+  }
+
+  @Test
+  public void testTypeString() {
+    for (int i = 0; i < 100; i++) {
+      assertEquals("Boolean[" + i + "]", new BooleanArrayEntry("", new boolean[i]).getType());
+    }
+  }
+
+  @Test
+  public void testDisplayString() {
+    boolean[] array = new boolean[65535];
+    for (int i = 0; i < array.length; i++) {
+      array[i] = Math.random() < 0.5;
+    }
+    BooleanArrayEntry entry = new BooleanArrayEntry("", array);
+    assertEquals(Arrays.toString(array), entry.getDisplayString());
+  }
+
+}
