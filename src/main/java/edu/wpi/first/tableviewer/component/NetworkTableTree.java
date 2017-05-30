cd554857abe7fef9fec45ae0831e041bc08a6b0e
Sam Carlberg <sam.carlberg@gmail.com>
2017-05-30 17:39:51 -0400

    Separate NetworkTableTree from ntcore

--- src/main/java/edu/wpi/first/tableviewer/component/NetworkTableTree.java
+++ src/main/java/edu/wpi/first/tableviewer/component/NetworkTableTree.java
@@ -1,20 +1,14 @@
 package edu.wpi.first.tableviewer.component;
 
-import edu.wpi.first.tableviewer.Filterable;
 import edu.wpi.first.tableviewer.entry.Entry;
 import edu.wpi.first.tableviewer.entry.TableEntry;
-import edu.wpi.first.wpilibj.networktables.NetworkTablesJNI;
 import edu.wpi.first.wpilibj.tables.ITable;
 import javafx.application.Platform;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.scene.control.TreeItem;
-import javafx.scene.control.TreeTableView;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,10 +16,7 @@ import java.util.stream.Stream;
  * A tree table view specifically for viewing network table entries. This is filterable
  * to allow metadata to be hidden and for entries to be searchable.
  */
-public class NetworkTableTree extends TreeTableView<Entry> implements Filterable<Entry> {
-
-  private final ObjectProperty<Predicate<Entry>> filter
-      = new SimpleObjectProperty<>(this, "filter", null);
+public class NetworkTableTree extends FilterableTreeTable<Entry> {
 
   // node comparators
   private static final Comparator<TreeItem<Entry>> branchesFirst
@@ -33,95 +24,49 @@ public class NetworkTableTree extends TreeTableView<Entry> implements Filterable
   private static final Comparator<TreeItem<Entry>> alphabetical
       = Comparator.comparing(i -> i.getValue().getKey());
 
-  private TreeItem<Entry> realRoot = null;
-
   /**
    * Creates a new network table tree. The tree is set up to show changes to network tables
    * in real time.
    */
   public NetworkTableTree() {
     super();
-    rootProperty().addListener(__ -> {
-      if (getFilter() == null || realRoot == null) {
-        realRoot = getRoot();
-      }
-    });
-    filter.addListener(__ -> updateItemsFromFilter());
     setSortPolicy(param -> {
-      if (realRoot != null) {
-        sort(realRoot);
+      if (getRealRoot() != null) {
+        sort(getRealRoot());
         return true;
       }
       return false;
     });
-    NetworkTablesJNI
-        .addEntryListener("", (uid, key, value, flags) -> makeBranches(key, value, flags), 0xFF);
   }
 
   /**
-   * Updates the items in this tree to only show items that pass the filter.
+   * Updates this tree based on information about a specific entry in network tables. This is
+   * intended to be called directly from a NetworkTablesJNI entry listener function. This method
+   * is smart enough to always run on the JavaFX application thread.
+   *
+   * <p>Example use to update the tree whenever any entry updates:
+   * <pre><code>
+   * NetworkTablesJNI.addEntryListener(
+   *       "",
+   *       (uid, key, value, flags) -> updateFromNetworkTables(key, value, flags),
+   *       0xFF);
+   * </code></pre>
+   *
+   * @param key   the key of the entry that updated
+   * @param value the new value of the update entry
+   * @param flags the flags for the updated entry
    */
-  public void updateItemsFromFilter() {
-    if (realRoot == null) {
-      // no items
-      return;
-    }
-    TreeItem<Entry> root = realRoot;
-    TreeItem<Entry> filteredRoot = new TreeItem<>(root.getValue(), root.getGraphic());
-    filteredRoot.setExpanded(root.isExpanded());
-    setRoot(filter(root, getFilter(), filteredRoot));
-  }
-
-  private static <T> TreeItem<T> filter(TreeItem<T> allRoot,
-                                        Predicate<? super T> filter,
-                                        TreeItem<T> filteredRoot) {
-    if (filter == null) {
-      // no filter
-      return allRoot;
-    }
-    for (TreeItem<T> child : allRoot.getChildren()) {
-      // Copy the child to avoid mucking with the real tree
-      TreeItem<T> filteredChild = new TreeItem<>();
-      filteredChild.setValue(child.getValue());
-      filteredChild.setExpanded(child.isExpanded());
-      // Recurse; filter children
-      filteredChild = filter(child, filter, filteredChild);
-      if (matches(child, filter)) {
-        filteredRoot.getChildren().add(filteredChild);
-      }
-    }
-    return filteredRoot;
-  }
-
-  private static <T> boolean matches(TreeItem<T> treeItem, Predicate<? super T> predicate) {
-    if (treeItem.isLeaf()) {
-      return predicate.test(treeItem.getValue());
-    } else {
-      return treeItem.getChildren().stream().anyMatch(c -> matches(c, predicate));
-    }
-  }
-
-  @Override
-  public void setFilter(Predicate<Entry> filter) {
-    this.filter.setValue(filter);
-  }
-
-  @Override
-  public Predicate<Entry> getFilter() {
-    return filter.getValue();
-  }
-
   @SuppressWarnings("unchecked")
-  private void makeBranches(String key, Object value, int flags) {
+  public void updateFromNetworkTables(String key, Object value, int flags) {
     if (!Platform.isFxApplicationThread()) {
-      Platform.runLater(() -> makeBranches(key, value, flags));
+      Platform.runLater(() -> updateFromNetworkTables(key, value, flags));
       return;
     }
     boolean deleted = (flags & ITable.NOTIFY_DELETE) != 0;
     List<String> pathElements = Stream.of(key.split("/"))
                                       .filter(s -> !s.isEmpty())
                                       .collect(Collectors.toList());
-    TreeItem<Entry> current = realRoot;
+    TreeItem<Entry> current = getRealRoot();
     TreeItem<Entry> parent;
     StringBuilder path = new StringBuilder();
     for (int i = 0; i < pathElements.size(); i++) {
