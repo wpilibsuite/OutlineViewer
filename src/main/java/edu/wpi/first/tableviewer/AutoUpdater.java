a437dd898880c723c284536cac397d31506840e6
Sam Carlberg <sam.carlberg@gmail.com>
2017-05-30 00:49:01 -0400

    Fix some networking issues. Use NetworkTable.setTeam when approriate
    Log changes to preferences

--- src/main/java/edu/wpi/first/tableviewer/AutoUpdater.java
+++ src/main/java/edu/wpi/first/tableviewer/AutoUpdater.java
@@ -1,5 +1,7 @@
 package edu.wpi.first.tableviewer;
 
+import edu.wpi.first.wpilibj.networktables.NetworkTable;
+
 /**
  * Class for automatically updating ntcore client/server status when preferences are changed.
  */
@@ -22,9 +24,21 @@ public class AutoUpdater {
    */
   public void update() {
     if (Prefs.isServer()) {
+      System.out.println("Starting server on port " + Prefs.getPort());
       NetworkTableUtils.setServer(Prefs.getPort());
     } else {
-      NetworkTableUtils.setClient(Prefs.getResolvedAddress(), Prefs.getPort());
+      String rawAddress = Prefs.getIp();
+      if (rawAddress.matches("[0-9]{1,5}")) {
+        // Treat as team number. Ignore the port and resolved address to use the 5 addresses
+        // used with NetworkTable.setTeam
+        System.out.println("Connecting to server for team " + rawAddress);
+        NetworkTableUtils.shutdown();
+        NetworkTable.setTeam(Integer.parseInt(rawAddress));
+      } else {
+        // Treat as a general address:port
+        System.out.println("Connecting to server " + Prefs.getResolvedAddress() + ":" + Prefs.getPort());
+        NetworkTableUtils.setClient(Prefs.getResolvedAddress(), Prefs.getPort());
+      }
     }
   }
 
