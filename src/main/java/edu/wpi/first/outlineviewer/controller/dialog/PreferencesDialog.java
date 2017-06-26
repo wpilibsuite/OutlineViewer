9861f3d764f94a546d3a488c169ff253de108f3b
Austin Shalit <austinshalit@gmail.com>
2017-06-25 18:12:46 -0700

    Refactor into MVC style

---
+++ src/main/java/edu/wpi/first/outlineviewer/controller/dialog/PreferencesDialog.java
@@ -0,0 +1,43 @@
+package edu.wpi.first.outlineviewer.controller.dialog;
+
+import edu.wpi.first.outlineviewer.OutlineViewer;
+import edu.wpi.first.outlineviewer.controller.PreferencesController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+/**
+ * A dialog for changing the app preferences.
+ */
+public class PreferencesDialog extends Dialog<ButtonType> {
+
+  private final PreferencesController controller;
+
+  /**
+   * Creates a new preferences dialog with the given title and button types.
+   *
+   * @param title       the title of the dialog
+   * @param buttonTypes the types of buttons for the dialog to use
+   * @throws IOException if the preferences FXML could not be loaded
+   */
+  public PreferencesDialog(String title, ButtonType... buttonTypes) throws IOException {
+    setTitle(title);
+    FXMLLoader loader = new FXMLLoader(OutlineViewer.class.getResource("Preferences.fxml"));
+    Pane prefsPane = loader.load();
+    controller = loader.getController();
+    getDialogPane().setContent(prefsPane);
+    getDialogPane().getButtonTypes().addAll(buttonTypes);
+    setResultConverter(x -> x);
+  }
+
+  /**
+   * Gets the controller for the pane.
+   */
+  public PreferencesController getController() {
+    return controller;
+  }
+
+}
---
+++ src/main/java/edu/wpi/first/outlineviewer/view/NetworkTableTree.java
@@ -0,0 +1,132 @@
+package edu.wpi.first.outlineviewer.view;
+
+import edu.wpi.first.outlineviewer.model.Entry;
+import edu.wpi.first.outlineviewer.model.TableEntry;
+import edu.wpi.first.wpilibj.tables.ITable;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.scene.control.TreeItem;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * A tree table view specifically for viewing network table entries. This is filterable
+ * to allow metadata to be hidden and for entries to be searchable.
+ */
+public class NetworkTableTree extends FilterableTreeTable<Entry> {
+
+  // node comparators
+  private static final Comparator<TreeItem<Entry>> branchesFirst
+      = (o1, o2) -> o1.isLeaf() ? (o2.isLeaf() ? 0 : 1) : -1;
+  private static final Comparator<TreeItem<Entry>> alphabetical
+      = Comparator.comparing(i -> i.getValue().getKey());
+
+  /**
+   * Creates a new network table tree. The tree is set up to show changes to network tables
+   * in real time.
+   */
+  public NetworkTableTree() {
+    super();
+    setSortPolicy(param -> {
+      if (getRealRoot() != null) {
+        sort(getRealRoot());
+        return true;
+      }
+      return false;
+    });
+  }
+
+  /**
+   * Updates this tree based on information about a specific model in network tables. This is
+   * intended to be called directly from a NetworkTablesJNI model listener function. This method
+   * is smart enough to always run on the JavaFX application thread.
+   *
+   * <p>Example use to update the tree whenever any model updates:
+   * <pre><code>
+   * NetworkTablesJNI.addEntryListener(
+   *       "",
+   *       (uid, key, value, flags) -> updateFromNetworkTables(key, value, flags),
+   *       0xFF);
+   * </code></pre>
+   *
+   * @param key   the key of the model that updated
+   * @param value the new value of the update model
+   * @param flags the flags for the updated model
+   */
+  @SuppressWarnings({"PMD.AvoidInstantiatingObjectsInLoops", "unchecked"})
+  public void updateFromNetworkTables(String key, Object value, int flags) {
+    if (!Platform.isFxApplicationThread()) {
+      Platform.runLater(() -> updateFromNetworkTables(key, value, flags));
+      return;
+    }
+    boolean deleted = (flags & ITable.NOTIFY_DELETE) != 0;
+    List<String> pathElements = Stream.of(key.split("/"))
+                                      .filter(s -> !s.isEmpty())
+                                      .collect(Collectors.toList());
+    TreeItem<Entry> current = getRealRoot();
+    TreeItem<Entry> parent = current;
+    StringBuilder path = new StringBuilder();
+    for (int i = 0; i < pathElements.size(); i++) {
+      String pathElement = pathElements.get(i);
+      path.append('/').append(pathElement);
+      parent = current;
+      current = current.getChildren()
+                       .stream()
+                       .filter(item -> item.getValue().getKey().equals(path.toString()))
+                       .findFirst()
+                       .orElse(null);
+      if (deleted) {
+        if (current == null) {
+          break;
+        } else if (i == pathElements.size() - 1) {
+          // last
+          parent.getChildren().remove(current);
+        }
+      } else if (i == pathElements.size() - 1) {
+        if (current == null) {
+          current = new TreeItem<>(Entry.entryFor(key, value));
+          parent.getChildren().add(current);
+        } else {
+          current.getValue().setValue(value);
+        }
+      } else if (current == null) {
+        current = new TreeItem<>(new TableEntry(path.toString()));
+        current.setExpanded(true);
+        parent.getChildren().add(current);
+      }
+    }
+    // Remove any empty subtables
+    if (deleted) {
+      for (TreeItem<Entry> item = parent; item != getRealRoot();) {
+        if (item.getValue() instanceof TableEntry && item.getChildren().isEmpty()) {
+          TreeItem<Entry> next = item.getParent();
+          item.getParent().getChildren().remove(item);
+          item = next;
+        } else {
+          // No more empty tables, bail
+          break;
+        }
+      }
+    }
+    sort();
+    updateItemsFromFilter();
+  }
+
+  /**
+   * Sorts tree nodes recursively in order of branches before leaves, then alphabetically.
+   *
+   * @param node the root node to sort
+   */
+  private void sort(TreeItem<Entry> node) {
+    if (!node.isLeaf()) {
+      boolean wasExpanded = node.isExpanded();
+      FXCollections.sort(node.getChildren(), branchesFirst.thenComparing(alphabetical));
+      node.getChildren().forEach(this::sort);
+      node.setExpanded(wasExpanded);
+    }
+  }
+
+}
