f403bc831b1843c8b7fd7f290c4bdc5650719006
Ryan Benasutti <firey45@gmail.com>
2017-10-25 13:15:50 -0400

    Fixed AddBooleanArrayDialogTest

--- src/test/java/edu/wpi/first/outlineviewer/view/dialog/AddBooleanArrayDialogTest.java
+++ src/test/java/edu/wpi/first/outlineviewer/view/dialog/AddBooleanArrayDialogTest.java
@@ -4,6 +4,7 @@ import com.google.common.primitives.Booleans;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 import javafx.scene.control.ListView;
+import javafx.util.Pair;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
@@ -20,8 +21,13 @@ class AddBooleanArrayDialogTest extends AddEntryArrayDialogTest<AddBooleanArrayD
     final Boolean[] test = new Boolean[]{false, true, false, true};
     dialog.setInitial(test);
 
-    assertArrayEquals(Booleans.toArray(Arrays.stream(test).collect(Collectors.toList())),
-        Booleans.toArray(((ListView) lookup(".list-view").query()).getItems()));
+    assertArrayEquals(
+        Booleans.toArray(Arrays.stream(test)
+            .collect(Collectors.toList())),
+        Booleans.toArray(((ListView<Pair<Integer, Boolean>>) lookup(".list-view").query())
+            .getItems().stream()
+            .map(Pair::getValue)
+            .collect(Collectors.toList())));
   }
 
   @Test
