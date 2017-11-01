c26e1081a1265ecb4c16e9275e86c7760f686731
Austin Shalit <austinshalit@gmail.com>
2017-10-31 23:25:14 -0400

    Java 8

--- src/main/java/edu/wpi/first/outlineviewer/OutlineViewer.java
+++ src/main/java/edu/wpi/first/outlineviewer/OutlineViewer.java
@@ -16,19 +16,16 @@ public class OutlineViewer extends Application {
 
   private static final ButtonType START = new ButtonType("Start", ButtonData.OK_DONE);
   private static final ButtonType QUIT = new ButtonType("Quit", ButtonData.CANCEL_CLOSE);
-  private static final String REQUIRED_JRE_VERSION = "1.8";
-  private static final String JRE_VERSION = System.getProperty("java.specification.version");
 
   @Override
   public void start(Stage primaryStage) throws IOException {
-    if (!REQUIRED_JRE_VERSION.equals(JRE_VERSION)) {
+    if (!"1.8".equals(System.getProperty("java.specification.version"))) {
       Alert invalidVersionAlert = new Alert(Alert.AlertType.ERROR);
       invalidVersionAlert.setHeaderText("Invalid JRE Version!");
       invalidVersionAlert.setContentText(
           String.format("You are using an unsupported Java version: %s!  "
-              + "Please download Java %s.",
-              System.getProperty("java.specification.version"),
-              REQUIRED_JRE_VERSION));
+              + "Please download Java 8.",
+              System.getProperty("java.version")));
       invalidVersionAlert.showAndWait();
 
       return;
