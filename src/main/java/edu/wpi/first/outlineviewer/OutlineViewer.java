c6d56df9ae3478844ad801b6c64a27bbb135bc6e
Austin Shalit <austinshalit@gmail.com>
2017-10-31 23:18:55 -0400

    Block launching the application with Java 9

--- src/main/java/edu/wpi/first/outlineviewer/OutlineViewer.java
+++ src/main/java/edu/wpi/first/outlineviewer/OutlineViewer.java
@@ -4,6 +4,7 @@ import edu.wpi.first.outlineviewer.view.dialog.PreferencesDialog;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonBar.ButtonData;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.Pane;
@@ -15,9 +16,24 @@ public class OutlineViewer extends Application {
 
   private static final ButtonType START = new ButtonType("Start", ButtonData.OK_DONE);
   private static final ButtonType QUIT = new ButtonType("Quit", ButtonData.CANCEL_CLOSE);
+  private static final String REQUIRED_JRE_VERSION = "1.8";
+  private static final String JRE_VERSION = System.getProperty("java.specification.version");
 
   @Override
   public void start(Stage primaryStage) throws IOException {
+    if (!REQUIRED_JRE_VERSION.equals(JRE_VERSION)) {
+      Alert invalidVersionAlert = new Alert(Alert.AlertType.ERROR);
+      invalidVersionAlert.setHeaderText("Invalid JRE Version!");
+      invalidVersionAlert.setContentText(
+          String.format("You are using an unsupported Java version: %s!  "
+              + "Please download Java %s.",
+              System.getProperty("java.specification.version"),
+              REQUIRED_JRE_VERSION));
+      invalidVersionAlert.showAndWait();
+
+      return;
+    }
+
     AutoUpdater updater = new AutoUpdater();
 
     PreferencesDialog preferencesDialog = new PreferencesDialog(QUIT, START);
