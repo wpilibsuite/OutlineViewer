9d37b99e95fc39e39f994557f6d7ba6750c812ca
Austin Shalit <austinshalit@gmail.com>
2019-05-12 09:01:06 -0400

    Update to spotless 3.23.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
     pmd
     id("edu.wpi.first.wpilib.versioning.WPILibVersioningPlugin") version "2.3"
     id("com.github.johnrengelman.shadow") version "5.0.0"
-    id("com.diffplug.gradle.spotless") version "3.13.0"
+    id("com.diffplug.gradle.spotless") version "3.23.0"
     id("com.github.spotbugs") version "1.6.4"
 }
 
